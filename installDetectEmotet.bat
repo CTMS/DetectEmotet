29f70edea42feaf86d42dd2f3281efa1f34aa46d
mjimerson <maria.jimerson88@gmail.com>
2018-05-17 12:19:19 -0400

    minor edits to install script

--- installDetectEmotet.bat
+++ installDetectEmotet.bat
@@ -12,9 +12,8 @@ cd c:\ctms\git_repos
 "C:\Program Files\Git\cmd\git.exe" clone https://github.com/CTMS/DetectEmotet.git
 
 cd DetectEmotet\resources
+schtasks /create /tn "Update DetectEmotet" /tr "C:\ctms\git_repos\DetectEmotet\resources\updateDetectEmotet.bat" /sc Weekly /d SAT /st 12:00 /ru "System"
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -File installPS40.ps1
 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "shutdown -r -t ([decimal]::round(((Get-Date).AddDays(1).Date.AddHours(3) - (Get-Date)).TotalSeconds))"
-
-schtasks /create /tn “Update DetectEmotet” /tr “C:\ctms\git_repos\DetectEmotet\resources\updateDetectEmotet.bat” /sc Weekly /d SAT /st 12:00 /ru “System”
+timeout /t 15
