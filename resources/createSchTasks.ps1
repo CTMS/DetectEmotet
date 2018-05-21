5f4f10d39750ce7dd723197789bcc9d6db784805
mjimerson <maria.jimerson88@gmail.com>
2018-05-21 12:04:25 -0400

    fixed task registation failure

--- resources/createSchTasks.ps1
+++ resources/createSchTasks.ps1
@@ -8,8 +8,7 @@ $cred = $Host.ui.PromptForCredential("Emotet Tasks Credentials", "Please enter c
 $sta1 = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File C:\ctms\git_repos\DetectEmotet\src\detect_emotet-continous.ps1"
 $stt1 = New-ScheduledTaskTrigger -Daily -At 12:00
 $sts1 = New-ScheduledTaskSettingsSet -MultipleInstances IgnoreNew
-$st1 = New-ScheduledTask -Action $sta1 -Trigger $stt1 -Settings $sts1
-$st1 | Register-ScheduledTask -TaskName "Detect Emotet" -Force -User $cred.username -Password $cred.getNetworkCredential().Password -RunLevel Highest
+Register-ScheduledTask -TaskName "Detect Emotet" -Force -Action $sta1 -Trigger $stt1 -Settings $sts1 -User $cred.username -Password $cred.getNetworkCredential().Password -RunLevel Highest
 
 # Scheduled Task to run Update script
 #   Runs Emotet Update script
@@ -19,5 +18,4 @@ $st1 | Register-ScheduledTask -TaskName "Detect Emotet" -Force -User $cred.usern
 $sta2 = New-ScheduledTaskAction -Execute "C:\ctms\git_repos\DetectEmotet\resources\updateDetectEmotet.bat"
 $sta2 = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Saturday -At 12:00
 $sts2 = New-ScheduledTaskSettingsSet -MultipleInstances IgnoreNew
-$st2 = New-ScheduledTask -Action $sta2 -Trigger $sta2 -Settings $sts2
-$st2 | Register-ScheduledTask -TaskName "Update Detect Emotet" -Force -User $cred.username -Password $cred.getNetworkCredential().Password -RunLevel Highest
+Register-ScheduledTask -TaskName "Update Detect Emotet" -Force -Action $sta2 -Trigger $sta2 -Settings $sts2 -User $cred.username -Password $cred.getNetworkCredential().Password -RunLevel Highest
